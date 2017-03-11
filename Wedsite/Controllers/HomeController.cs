8bc27c19a26e0f2b592eda5fd213368348db0c6f
HarmzNaN <hsncool@hotmail.com>
2017-03-11 01:09:17 +0000

    Hello world

---
+++ Wedsite/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wedsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Hello World!";
+            return View();
+        }
+    }
+}
